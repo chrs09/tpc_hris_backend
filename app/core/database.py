5a1c1f30a1c0ad967d3efa60a22e2657b327d926
ianapuhin <epoxapuhin@gmail.com>
2026-02-18 17:22:00 +0800

    modify database.py

--- app/core/database.py
+++ app/core/database.py
@@ -1,17 +1,26 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.core.config import settings
+import os
 
-engine = create_engine(
-    settings.DATABASE_URL,
-    pool_pre_ping=True,
-    connect_args={
-        "ssl": {"ssl": {"ssl_mode": "REQUIRED"}}
-    }
-)
+DATABASE_URL = settings.DATABASE_URL
+
+# Enable SSL only when running in Azure
+if os.getenv("WEBSITE_HOSTNAME"):
+    engine = create_engine(
+        DATABASE_URL,
+        pool_pre_ping=True,
+        connect_args={
+            "ssl": {"fake_flag_to_enable_tls": True}
+        }
+    )
+else:
+    engine = create_engine(
+        DATABASE_URL,
+        pool_pre_ping=True
+    )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
 
