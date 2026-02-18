18950182c29a1f07fe06e7d34d8d9ca0c459c5e1
ianapuhin <epoxapuhin@gmail.com>
2026-02-18 16:26:09 +0800

    modify database.py add ssl on configuration

--- app/core/database.py
+++ app/core/database.py
@@ -2,7 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.core.config import settings
 
-engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True)
+engine = create_engine(
+    settings.DATABASE_URL,
+    pool_pre_ping=True,
+    connect_args={
+        "ssl": {"ssl": {"ssl_mode": "REQUIRED"}}
+    }
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
